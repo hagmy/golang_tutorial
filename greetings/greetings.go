ef78973cf8f9f7cd845bed7758cfa7e2a3b3ad0d
hagmy <hagamiya238@gmail.com>
2023-07-30 00:27:24 +0900

    [ch04] Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// := operator is a shortcut for declaring and initializing a variable in one line.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	// nil (meaning no error) as a second value in the successful return.
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("hagmy")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message and print it.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the consle
+	// and exist the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to the consle
 	fmt.Println(message)
 }
