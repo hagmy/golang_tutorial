a3f917fe91280912d96ae9c534a343b7db83fdcb
hagmy <hagamiya238@gmail.com>
2023-07-30 11:26:41 +0900

    [ch03-01] Get a database handle and connect

---
+++ ch03-accessing-a-relational-database/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func main() {
+	var err error
+
+	// Loading env file.
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Capture connection properies.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+	// Get a database handle.
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
