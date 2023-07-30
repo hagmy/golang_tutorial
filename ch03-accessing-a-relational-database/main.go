500f4429d0a5638e010e9fd0d2e35283da107784
hagmy <hagamiya238@gmail.com>
2023-07-30 13:20:40 +0900

    [ch03-03] Query for a single row

--- ch03-accessing-a-relational-database/main.go
+++ ch03-accessing-a-relational-database/main.go
@@ -54,6 +54,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Alubms found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -83,4 +89,17 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsByID %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsByID %d: %v", id, err)
+	}
+	return alb, nil
 }
