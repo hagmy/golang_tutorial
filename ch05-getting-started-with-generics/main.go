960fb37957361ca734cef393427e8bd672384529
hagmy <hagamiya238@gmail.com>
2023-08-05 19:26:14 +0900

    [ch05-2] Add a generic function to handle multiple types

--- ch05-getting-started-with-generics/main.go
+++ ch05-getting-started-with-generics/main.go
@@ -15,23 +15,31 @@ func main() {
 
 	fmt.Printf(
 		"Non-Generic Sums: %v and %v\n",
-		SumInts(ints),
-		SumFloats(floats),
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats),
 	)
 }
 
-func SumInts(m map[string]int64) int64 {
-	var s int64
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
 	return s
 }
 
-func SumFloats(m map[string]float64) float64 {
-	var s float64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
+// func SumInts(m map[string]int64) int64 {
+// 	var s int64
+// 	for _, v := range m {
+// 		s += v
+// 	}
+// 	return s
+// }
+
+// func SumFloats(m map[string]float64) float64 {
+// 	var s float64
+// 	for _, v := range m {
+// 		s += v
+// 	}
+// 	return s
+// }
