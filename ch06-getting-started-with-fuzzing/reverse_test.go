2baa230c60a9d960a8c113f63b9f3448c8a3d354
hagmy <hagamiya238@gmail.com>
2023-08-06 10:06:58 +0900

    [ch05-3] Add a fuzz test

--- ch06-getting-started-with-fuzzing/reverse_test.go
+++ ch06-getting-started-with-fuzzing/reverse_test.go
@@ -1,7 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"unicode/utf8"
+)
 
+// unit test
 func TestReverse(t *testing.T) {
 	testcases := []struct {
 		in, want string
@@ -17,4 +21,22 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+// fuzz test
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "12345!"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q\n", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
